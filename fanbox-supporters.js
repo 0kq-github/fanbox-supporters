59fb845470c5af109291b3f6078e7815661ed5da
0kq <0kq@0kqnet.work>
2023-07-13 20:13:45 +0900

    fix alert

--- fanbox-supporters.js
+++ fanbox-supporters.js
@@ -1,24 +1,24 @@
 function main() {
-    if (!location.href.match("https://www.fanbox.cc/manage/relationships")) {
+    if (location.href.match("https://www.fanbox.cc/manage/relationships")) {
+        const result = {};
+        const supporters = document.querySelectorAll("a[href*='/manage/relationships/']");
+        supporters.forEach((element) => {
+            const url = element.href.split("/");
+            result[url[url.length - 1]] = {
+                "name":element.parentNode.parentNode.querySelectorAll("div > div")[0].innerText,
+                "status":element.parentNode.parentNode.querySelectorAll("div > div")[1].innerText,
+                "since":element.parentNode.parentNode.querySelectorAll("div > div")[3].innerText,
+                "memo":element.parentNode.parentNode.querySelectorAll("div > div")[4].innerText
+            }
+        });
+        const blob = new Blob([JSON.stringify(result)], { type: "text/plain" });
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = "fanbox.json"; link.click();
+        alert("支援者数 " + supporters.length.toString() + " 人")
+    } else {
         alert("支援者管理ページを開いて実行してください");
-        return;
     }
-    const result = {};
-    const supporters = document.querySelectorAll("a[href*='/manage/relationships/']");
-    supporters.forEach((element) => {
-        const url = element.href.split("/");
-        result[url[url.length - 1]] = {
-            "name":element.parentNode.parentNode.querySelectorAll("div > div")[0].innerText,
-            "status":element.parentNode.parentNode.querySelectorAll("div > div")[1].innerText,
-            "since":element.parentNode.parentNode.querySelectorAll("div > div")[3].innerText,
-            "memo":element.parentNode.parentNode.querySelectorAll("div > div")[4].innerText
-        }
-    });
-    const blob = new Blob([JSON.stringify(result)], { type: "text/plain" });
-    const link = document.createElement("a");
-    link.href = URL.createObjectURL(blob);
-    link.download = "fanbox.json"; link.click();
-    alert("支援者数 " + supporters.length.toString() + " 人")
 }
 
 export { main };

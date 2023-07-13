77e9f6ede70f64114eab4d2414c817f46a92c6de
0kq <0kq@0kqnet.work>
2023-07-13 18:01:33 +0900

    update

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-javascript: (
+javascript:(
     function () {
         let result = {};
         let supporters = document.querySelectorAll("a[href*='/manage/relationships/']");
         supporters.forEach((element) => {
             let url = element.href.split("/");
             result[url[url.length - 1]] = {
+                "name":element.parentNode.parentNode.querySelectorAll("div > div")[0].innerText,
                 "status":element.parentNode.parentNode.querySelectorAll("div > div")[1].innerText,
-                "since":element.parentNode.parentNode.querySelectorAll("div > div")[3].innerText
+                "since":element.parentNode.parentNode.querySelectorAll("div > div")[3].innerText,
+                "memo":element.parentNode.parentNode.querySelectorAll("div > div")[4].innerText
             }
         });
         let blob = new Blob([JSON.stringify(result)], { type: "text/plain" });
@@ -15,4 +17,4 @@ javascript: (
         link.download = "fanbox.json"; link.click();
         alert("Downloading " + supporters.length.toString() + " Supporters...")
     }
-)();
+());

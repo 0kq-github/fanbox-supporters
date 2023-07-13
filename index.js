59a5bd84a1eaedefb0bbe5d58f066473c13a1988
0kq <0kq@0kqnet.work>
2023-07-13 17:01:48 +0900

    add index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+javascript: (
+    function () {
+        let result = {};
+        let supporters = document.querySelectorAll("a[href*='/manage/relationships/']");
+        supporters.forEach((element) => {
+            let url = element.href.split("/");
+            result[url[url.length - 1]] = {
+                "status":element.parentNode.parentNode.querySelectorAll("div > div")[1].innerText,
+                "date":element.parentNode.parentNode.querySelectorAll("div > div")[3].innerText
+            }
+        });
+        let blob = new Blob([JSON.stringify(result)], { type: "text/plain" });
+        let link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = "fanbox.json"; link.click();
+        alert("Downloading " + supporters.length.toString() + " Supporters...")
+    }
+)();
